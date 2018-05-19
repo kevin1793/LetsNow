10a86df4c041d8272daeeaa03cf5ab4f798b6540
Kevin Claveria <kevin1793@gmail.com>
2018-05-19 02:38:53 -0500

    a

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
+
+	login(){
+		let provider = new firebase.auth.FacebookAuthProvider();
+
+		firebase.auth().signInWithRedirect(provider).then( () => {
+			firebase.auth().getRedirectResult().then((result) => {
+				alert(JSON.stringify(result));
+			}).catch(function(error) {
+				alert(JSON.stringify(error))
+			});
+		})
+	}
 
 }
